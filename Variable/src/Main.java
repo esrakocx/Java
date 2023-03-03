4771a1c49f26e65920c639aa60ebe1a2c71ea191
Esra Koc <90002015+esrakocx@users.noreply.github.com>
2023-03-03 23:31:28 +0300

    Intro to Jav

---
+++ Variable/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+	public static void main(String[] args) {
+		int a = 12;
+		int b = 23;
+		int c = 5;
+		
+		int result = a + b + 2 * c;
+		System.out.println("Result: " + result);
+	}
+}
