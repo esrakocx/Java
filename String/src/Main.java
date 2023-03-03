54cb4d4abc3f14471a08ab646ec17d639e1a1e9b
Esra Koc <90002015+esrakocx@users.noreply.github.com>
2023-03-03 23:27:27 +0300

    String data type

---
+++ String/src/Main.java
@@ -0,0 +1,42 @@
+
+public class Main {
+	public static void main(String[] args) {
+		
+		String a = "Hello";
+		System.out.println(a + " Welcome!");
+		
+		String b = "Java ";
+		String c = "Programming ";
+		String d = "Language";
+		
+		String sum = b + c + d;
+		System.out.println("Sum: " + sum);
+		
+		String e = "Hi";
+		int f = 34;
+		String g = e + f;
+		System.out.println(g);
+		
+		double h = 3.45;
+		byte i = 4;
+		e = e + h + i;
+		System.out.println(e);
+		
+		char j = '?';
+		String k = "Hello, how are you" + j;
+		System.out.println(k);
+		
+		String l = "Java " + 
+				"Programming " + 
+				"Language";
+		System.out.println(l);
+		
+		String m = "Java\tProgramming\tLanguage";
+		System.out.println(m);
+		
+		String n = "Java\nProgramming\nLanguage";
+		System.out.println(n);
+		
+	}
+
+}
