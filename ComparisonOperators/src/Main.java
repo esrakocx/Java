a4c5f8bfafee6e5fe228c1c710dacc0167a3059a
Esra Koc <90002015+esrakocx@users.noreply.github.com>
2023-03-08 12:43:02 +0300

    Comparison operators

---
+++ ComparisonOperators/src/Main.java
@@ -0,0 +1,25 @@
+
+public class Main {
+	public static void main(String[] args) {
+		
+		/*
+		 Comparison Operators: 
+		 
+		 == --> Is it equal?
+		 != --> Is not it equal?
+		 >  --> Is it big?
+		 >= --> Is it big or equal?
+		 <  --> Is it small?
+		 <= --> Is it small or equal?
+		 
+		 */
+		
+		System.out.println(3 == 3); //true
+		System.out.println(3 == 4); //false
+		System.out.println(3 != 4); //true
+		System.out.println(2 > 3); //false
+		System.out.println(3 >= 2); //true
+		System.out.println(2 < 3); //true
+		System.out.println(2 <= 3); //true
+	}
+}
