f6378ef9bdd8af2eb73646cb086892659b4e4d15
Esra Koc <90002015+esrakocx@users.noreply.github.com>
2023-03-28 13:08:16 +0300

    Introduction to OOP

---
+++ IntroToOOP/src/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+	public static void main(String[] args) {
+		
+		Car car1 = new Car();
+		car1.color = "Red";
+		car1.doors = 4;
+		car1.model = "A3";
+		car1.motor = "16V";
+		car1.wheels = 4;
+		
+		System.out.println(car1.model);
+	}
+}
